fb218f80ba3ab0f6366d171e19e8b0719e6a74ac
Brandy Rohrer <riversedgekayak@gmail.com>
2021-03-16 14:38:19 -0400

    First commit

---
+++ Source.cpp
@@ -0,0 +1,15 @@
+//Brandy Rohrer
+//March 16, 2021
+//CIS 1202 Section CR2
+//Source Code Management 1
+
+#include<iostream>
+using namespace std;
+
+int main() {
+
+	cout << "Hello World!" << endl;
+
+	system("pause");
+	return 0;
+}
